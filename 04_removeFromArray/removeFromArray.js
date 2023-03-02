624844d4a16005d029ecebc213a063da25258f1c
Roy Mera <myirra2002@gmail.com>
2023-03-02 14:17:28 +0900

    Adds exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
 
-};
+const removedArray = [];
+
+arr.forEach((item) => {
+    if (!args.includes(item)) {
+        removedArray.push(item);
+      }
+    });
+
+    return removedArray;
+  };
 
 // Do not edit below this line
 module.exports = removeFromArray;
