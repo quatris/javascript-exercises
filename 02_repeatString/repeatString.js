b42f542099ba6ac442d4536c777402edec4b7b7a
Roy Mera <myirra2002@gmail.com>
2023-03-02 12:54:08 +0900

    Adds exercises 1&2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let repeatedString = "";
+    if (num < 0) {
+        repeatedString = "ERROR";
+        return repeatedString; }
+    else {    
+    while(num > 0) {
+        repeatedString += string;
+        num--;
+    }
 
-};
+        return repeatedString;
+    }
+    }
+    
+    
+    
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
