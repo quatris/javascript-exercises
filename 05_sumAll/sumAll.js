9cd5b445510ac0f439cbbdbab9630af1ae22c6ae
Roy Mera <myirra2002@gmail.com>
2023-03-06 11:08:01 +0900

    Implements sumAll loop exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+let sum = 0;
+min = Math.min(num1, num2);
+max = Math.max(num1, num2);
 
-};
+if (num1 < 0 || num2 < 0 || Number(num1) !== num1 || Number(num2) !== num2) {
+    return "ERROR"
+ } else {
+for (let i = min; i <= max; i++) {
+    sum += i;
+}
+return sum;
+}
+}
+
 
 // Do not edit below this line
 module.exports = sumAll;
