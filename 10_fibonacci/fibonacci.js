e6c1a1a241c619a5d9320007ce0dcc7c25670da7
Roy Mera <myirra2002@gmail.com>
2023-03-10 15:26:06 +0900

    Adds exercise 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(str) {
+    const n = parseInt(str);
+  
+    if (isNaN(n) || n < 0) {
+      return "OOPS";
+    }
+    
+    if (n <= 1) {
+      return n;
+    }
+    
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  };
 
 // Do not edit below this line
 module.exports = fibonacci;
