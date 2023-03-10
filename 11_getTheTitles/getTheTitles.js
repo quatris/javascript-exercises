a5f372d3be71cf84e5b378337c790b5ba2b43100
Roy Mera <myirra2002@gmail.com>
2023-03-10 15:31:27 +0900

    Adds exercise 11 getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,9 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function(books) {
+    if (!books) {
+        return [];
+      }
+      return books.map(book => book.title);
+    };
 
 // Do not edit below this line
 module.exports = getTheTitles;
