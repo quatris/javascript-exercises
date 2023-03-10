04c4c0bc5e0be8a09de51dac23cd829de1599003
Roy Mera <myirra2002@gmail.com>
2023-03-10 15:40:23 +0900

    Adds exercise 12 findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,31 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const getAge = function (birth, death) {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+    return death - birth;
+  };
 
+  return people.reduce((oldestPerson, currentPerson) => {
+    const oldestAge = getAge(
+      oldestPerson.yearOfBirth,
+      oldestPerson.yearOfDeath
+    );
+    const currentAge = getAge(
+      currentPerson.yearOfBirth,
+      currentPerson.yearOfDeath
+    );
+
+    if (
+      currentAge > oldestAge ||
+      oldestPerson === null ||
+      oldestPerson.name === null
+    ) {
+      return currentPerson;
+    } else {
+      return oldestPerson;
+    }
+  });
 };
 
 // Do not edit below this line
