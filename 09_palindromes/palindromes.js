364e473e505ddf78b6bfddda81b694ae176574cf
Roy Mera <myirra2002@gmail.com>
2023-03-10 15:18:46 +0900

    Adds exercise 9 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+ // Convert the string to lowercase and remove non-alphanumeric characters
+ str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+  
+ // Compare the string to its reverse
+ return str === str.split('').reverse().join('');
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
